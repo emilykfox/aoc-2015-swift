1a468d830f7a9f00192f41ce2ca03ef46067df79
Emily Fox <emilykfox@fastmail.com>
2025-09-22 15:55:24 -0500

    Add dependency on Swift Collections

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "AoC2015",
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-collections", "1.2.0" ..< "1.3.0"),
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.

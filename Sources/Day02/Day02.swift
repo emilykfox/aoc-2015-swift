6ebac06980b7be69a33d525b7c41c396370c4d71
Emily Fox <emilykfox@fastmail.com>
2025-09-23 09:24:07 -0500

    Remove Regex from Day 02

--- Sources/Day02/Day02.swift
+++ Sources/Day02/Day02.swift
@@ -1,37 +1,20 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
-import RegexBuilder
-
 @available(macOS 15, *)
 @main
 struct Day02 {
     static func main() {
-        let lengthRef = Reference(Int.self)
-        let widthRef = Reference(Int.self)
-        let heightRef = Reference(Int.self)
-        
-        let regex = Regex {
-            Anchor.startOfLine
-            TryCapture(as: lengthRef) {
-                OneOrMore(.digit)
-            } transform: { Int($0) }
-            "x"
-            TryCapture(as: widthRef) {
-                OneOrMore(.digit)
-            } transform: { Int($0) }
-            "x"
-            TryCapture(as: heightRef) {
-                OneOrMore(.digit)
-            } transform: { Int($0) }
-            Anchor.endOfLine
-        }
-
         var measurements: Array<(length: Int, width: Int, height: Int)> = []
         while let line = readLine(strippingNewline: true) {
-            if let lineMatch = line.wholeMatch(of: regex) {
-                measurements.append((length: lineMatch[lengthRef], width: lineMatch[widthRef], height: lineMatch[heightRef]))
+            let parts = line.split(separator: "x")
+            if parts.count != 3 {
+                continue
             }
+            guard let length = Int(parts[0]), let width = Int(parts[1]), let height = Int(parts[2]) else {
+                continue
+            }
+            measurements.append((length: length, width: width, height: height))
         }
 
         let paperNeeded = measurements.map { (length: Int, width: Int, height: Int) -> Int in
